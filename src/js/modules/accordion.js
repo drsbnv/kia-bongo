16ba85cf87b6bc54af127a999fe4d8ccab953a2a
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2022-10-07 12:07:04 +0500

    accordion

---
+++ src/js/modules/accordion.js
@@ -0,0 +1,15 @@
+const items = document.querySelectorAll('.accordion-item h3');
+console.log(items)
+items.forEach(item => {
+	item.addEventListener('click', e => {
+		if(!item.nextSibling.nextElementSibling.classList.contains('opacity-0')){
+			return
+		}
+		items.forEach(el => {
+			el.classList.remove('bg-black', 'text-white', 'pl-4', '!translate-x-0')
+			el.nextSibling.nextElementSibling.classList.add('opacity-0', 'h-0', '-traslate-y-3', 'invisible')
+		})
+		item.classList.add('bg-black', 'text-white', 'pl-4', '!translate-x-0')
+		item.nextSibling.nextElementSibling.classList.remove('opacity-0', 'h-0', '-traslate-y-3', 'invisible')
+	})
+})

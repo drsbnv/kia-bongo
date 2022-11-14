14979551cd0287947ff2074e7d09c9300d263eed
Alexander Sabanov <alexsab@mail.ru>
2022-11-14 17:25:52 +0400

    upd thumb on youtube

--- src/js/modules/videoReviews.js
+++ src/js/modules/videoReviews.js
@@ -6,45 +6,42 @@ let player;
 let videoId = null;
 
 const videos = [
-	{'videoId': 'EoLRTusjPvk', 'title': 'Kia bongo 3 4х4 отзывы реальных владельцев'},
-	{'videoId': '9nnxxkpNFFw', 'title': '2011 Киа Бонго 3 4x4. Обзор (интерьер, экстерьер, двигатель)'},
-	{'videoId': 'MmZ8tf1htYc', 'title': 'Kia Bongo 3, 4wd/Киа Бонго,Корейский грузовик,отзыв от Чемпиона Мира или НОВЫЙ ТЕСТ-ДРАЙВ от СЕРОГО'},
-	{'videoId': 'FNPY8RYEZBI', 'title': 'Kia Bongo 3 на заказ из Южной Кореи 4wd'},
-	{'videoId': '0JWcAUc-VcM', 'title': 'KIA BONGO 3, АВТООБЗОР дизель 2,9 турбо, 5лет в России'},
-	{'videoId': 'SyQsaNIWPP0', 'title': 'Обзор KIA Bongo 3 K4000 Рефрижераторный фургон +5С…-20С, фургон 12м3, 5 европаллет'},
-	{'videoId': 'spON6LceINw', 'title': 'Kia Bongo 3 Offroad tires (DOUBLE CAB ASH GRAY)'},
-	{'videoId': 'LpoQItNb_xk', 'title': 'EURO 4 : KIA BONGO 3 4X4 DOUBLE CAB FRONTDRIVE'},
+	{'id': 'EoLRTusjPvk', 'thumb': 'maxresdefault', 'title': 'Kia bongo 3 4х4 отзывы реальных владельцев'},
+	{'id': '9nnxxkpNFFw', 'thumb': 'maxresdefault', 'title': '2011 Киа Бонго 3 4x4. Обзор (интерьер, экстерьер, двигатель)'},
+	{'id': 'MmZ8tf1htYc', 'thumb': 'maxresdefault', 'title': 'Kia Bongo 3, 4wd/Киа Бонго,Корейский грузовик,отзыв от Чемпиона Мира или НОВЫЙ ТЕСТ-ДРАЙВ от СЕРОГО'},
+	{'id': 'FNPY8RYEZBI', 'thumb': 'maxresdefault', 'title': 'Kia Bongo 3 на заказ из Южной Кореи 4wd'},
+	{'id': '0JWcAUc-VcM', 'thumb': 'maxresdefault', 'title': 'KIA BONGO 3, АВТООБЗОР дизель 2,9 турбо, 5лет в России'},
+	{'id': 'SyQsaNIWPP0', 'thumb': 'hqdefault', 'title': 'Обзор KIA Bongo 3 K4000 Рефрижераторный фургон +5С…-20С, фургон 12м3, 5 европаллет'},
+	{'id': 'spON6LceINw', 'thumb': 'hqdefault', 'title': 'Kia Bongo 3 Offroad tires (DOUBLE CAB ASH GRAY)'},
+	{'id': 'LpoQItNb_xk', 'thumb': 'hqdefault', 'title': 'EURO 4 : KIA BONGO 3 4X4 DOUBLE CAB FRONTDRIVE'},
 ];
 
-function videoHtml(id, title, colSpan, slider = false){
-	let html;
-	if(!slider){
-		html = `
-		<div class="${colSpan}modal-video-btn" data-id="${id}" title="${title}">
-		<h3 class="uppercase whitespace-nowrap truncate mb-2 xl:mb-4 text-xs lg:text-base">${title}</h3>
-		<div class="pb-[56.25%] border border-black rounded-tl-3xl overflow-hidden cursor-pointer bg-cover bg-center transition-all duration-500 hover:-translate-x-2" style="background-image: url(https://img.youtube.com/vi/${id}/maxresdefault.jpg);"></div>
+function videoReviews(item, idx){
+	const colSpan = idx == 0 || idx == 1 ? 'col-span-2 ' : '';
+	return `
+	<div class="${colSpan}modal-video-btn" data-id="${item.id}" title="${item.title}">
+		<h3 class="uppercase whitespace-nowrap truncate mb-2 xl:mb-4 text-xs lg:text-base">${item.title}</h3>
+		<div class="pb-[56.25%] border border-black rounded-tl-3xl overflow-hidden cursor-pointer bg-cover bg-center transition-all duration-500 hover:-translate-x-2" style="background-image: url(https://img.youtube.com/vi/${item.id}/${item.thumb}.jpg);"></div>
+	</div>
+	`;
+}
+function videoSlider(item){
+	return `
+	<div class="swiper-slide">
+		<div class="modal-video-btn" data-id="${item.id}" title="${item.title}">
+			<div class="pb-[56.25%] border border-black rounded-tl-3xl overflow-hidden cursor-pointer bg-cover bg-center" style="background-image: url(https://img.youtube.com/vi/${item.id}/${item.thumb}.jpg);"></div>
+			<h3 class="text-center text-xs uppercase mt-4 px-12 line-clamp-2">${item.title}</h3>
 		</div>
-		`;
-	}else{
-		html = `
-		<div class="swiper-slide">
-			<div class="modal-video-btn" data-id="${id}" title="${title}">
-				<div class="pb-[56.25%] border border-black rounded-tl-3xl overflow-hidden cursor-pointer bg-cover bg-center" style="background-image: url(https://img.youtube.com/vi/${id}/maxresdefault.jpg);"></div>
-				<h3 class="text-center text-xs uppercase mt-4 px-12 line-clamp-2">${title}</h3>
-			</div>
-		</div>
-		`;
-	}
-	return html;
+	</div>
+	`;
 }
 
 videos.forEach((item, idx) => {
-	const colSpan = idx == 0 || idx == 1 ? 'col-span-2 ' : '';
-	document.getElementById('videoReviews')
-		.insertAdjacentHTML('beforeend', videoHtml(item.videoId, item.title, colSpan))
+	document.getElementById('video-reviews')
+		.insertAdjacentHTML('beforeend', videoReviews(item, idx))
 	document.getElementById('video-slider')
 		.querySelector('.swiper-wrapper')
-		.insertAdjacentHTML('beforeend', videoHtml(item.videoId, item.title, null, true));
+		.insertAdjacentHTML('beforeend', videoSlider(item));
 })
 
 document.querySelectorAll('.modal-video-btn').forEach(el => {
